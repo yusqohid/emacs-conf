99de95f0f618fe6bacfa4d413de2dd525f8f0f12
yusqohid <yusqo975@gmail.com>
2026-06-24 09:02:18 +0700

    add custom.el

---
+++ custom.el
@@ -0,0 +1,36 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("d12b1d9b0498280f60e5ec92e5ecec4b5db5370d05e787bc7cc49eae6fb07bc0"
+     "7771c8496c10162220af0ca7b7e61459cb42d18c35ce272a63461c0fc1336015"
+     "91db2df9490180a006964179f3aa4fcbc6bbf63cdcba189b41ea1ff5a606df33"
+     "b15bf9cabdd891f0c163d1b914e901d0d9f8f74ad4075b2b8e68a8f35247f82b"
+     "c8863bf29722cf36c5d293ac723fe9698e16f778326d791516dd717951ea8cd6"
+     "5244ba0273a952a536e07abaad1fdf7c90d7ebb3647f36269c23bfd1cf20b0b8"
+     "f700bc979515153bef7a52ca46a62c0aa519950cc06d539df4f3d38828944a2c"
+     "0325a6b5eea7e5febae709dab35ec8648908af12cf2d2b569bedc8da0a3a81c1"
+     "1fc299974daa270e19d1b206ec40aab3a0ce35e7c6a7d389b156bcd411e41a3c"
+     "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
+     "0d2c5679b6d087686dcfd4d7e57ed8e8aedcccc7f1a478cd69704c02e4ee36fe"
+     "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
+     "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326"
+     default))
+ '(package-selected-packages nil)
+ '(warning-suppress-types '((initialization)))
+ '(whitespace-style
+   '(face trailing tabs spaces lines newline missing-newline-at-eof empty
+          indentation space-after-tab space-before-tab space-mark
+          tab-mark)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
