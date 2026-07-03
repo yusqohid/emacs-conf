3936715a6ce5607271e69daf24e9ec98ea682b9a
yusqohid <yusqo975@gmail.com>
2026-07-03 16:14:51 +0700

    clean up

--- custom.el
+++ custom.el
@@ -4,7 +4,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(custom-safe-themes
+   '("6963de2ec3f8313bb95505f96bf0cf2025e7b07cefdb93e3d2e348720d401425"
+     "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326"
+     "9e5e0ff3a81344c9b1e6bfc9b3dcf9b96d5ec6a60d8de6d4c762ee9e2121dfb2"
+     "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
+     "e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7" default))
+ '(package-selected-packages
+   '(auctex base16-theme company corfu diff-hl doom-themes evil go-mode hl-todo
+            magit markdown-mode move-text multiple-cursors org-modern php-mode
+            treemacs undo-fu undo-fu-session web-mode yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- post-init.el
+++ post-init.el
@@ -1,20 +1,34 @@
 ;;; post-init.el --- DESCRIPTION -*- no-byte-compile: t; lexical-binding: t; -*-
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 (use-package corfu
-  :ensure t
   :init
-  (global-corfu-mode)
-  (corfu-auto t)
-  (corfu-auto-delay 0.1)
-  (corfu-auto-prefix 2))
+  (global-corfu-mode))
+;;  (setq corfu-auto t
+;;        corfu-auto-delay 0.4
+;;        corfu-quit-no-match 'separator))
+(use-package multiple-cursors
+  :bind
+  (("C->" . mc/mark-next-like-this)
+   ("C-<" . mc/mark-previous-like-this)
+   ("C-c C->" . mc/mark-all-like-this)))
+
+(use-package move-text
+  :bind (("M-<up>" . move-text-up)
+         ("M-<down>" . move-text-down)))
+
+(use-package evil
+  :bind (("C-c v" . evil-mode)))
+
+(use-package treemacs
+  :bind (("C-c o p" . treemacs)))
 
 (use-package yasnippet
-  :ensure t
-  :config
+  :init
   (yas-global-mode 1))
 
 (use-package diff-hl
-  :ensure t
   :hook ((prog-mode . diff-hl-mode)
          (text-mode . diff-hl-mode)
          (dired-mode . diff-hl-dired-mode))
@@ -22,18 +36,17 @@
   (diff-hl-flydiff-mode)
   (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh))
 
-(use-package evil
-  :ensure t
-  :init
-  (require 'evil))
+(use-package org-modern
+  :hook (org-mode . org-modern-mode))
 
 (use-package org
   :ensure nil
   :defer t
+  :hook (org-mode . org-indent-mode)
+  :init
+  (setq org-src-tab-acts-natively t
+        org-edit-src-content-indentation 0)
   :config
-  (setq org-src-fontify-natively t)
-  (setq org-src-tab-acts-natively t)
-  (setq org-edit-src-content-indentation 0)
   (add-to-list 'org-src-lang-modes '("rust" . rust-ts)))
 
 (use-package tex
@@ -54,34 +67,27 @@
   ;; compile dengan latexmk
   (setq TeX-command-default "LatexMk"))
 
-;; (use-package undo-fu
-;;   :ensure t)
-;;
-;; (use-package undo-fu-session
-;;   :ensure t
-;;   :after undo-fu
-;;   :config
-;;   (undo-fu-session-global-mode))
+(use-package undo-fu)
+(use-package undo-fu-session
+  :after undo-fu
+  :config
+  (undo-fu-session-global-mode))
 
 (use-package doom-themes
-  :ensure t
   :config
-  (load-theme 'doom-opera t)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+  (load-theme 'doom-pine t)
+  (doom-themes-org-config))  ;; Corrects (and improves) org-mode's native fontification.
 
 (use-package hl-todo
-  :ensure t
   :hook (prog-mode . hl-todo-mode))
 
 (use-package magit
-  :ensure t
   :commands magit)
 
 (use-package emacs
   :ensure nil
   :init
-  (add-to-list 'default-frame-alist '(font . "Iosevka Nerd Font-16"))
+  (add-to-list 'default-frame-alist '(font . "MesloLGM Nerd Font-16"))
   (column-number-mode 1)
   (repeat-mode 1)
   ;; (electric-pair-mode 1)
@@ -92,31 +98,37 @@
   (savehist-mode 1)
   (global-auto-revert-mode 1)
   (delete-selection-mode 1)
-  ;; (load-theme 'modus-vivendi t))
+  (save-place-mode 1)
+  ;;(load-theme 'modus-operandi t))
 
   :config
   ;; (windmove-default-keybindings 'control)
 
   (add-hook 'prog-mode-hook #'display-line-numbers-mode)
+  ;;(add-hook 'text-mode-hook #'display-line-numbers-mode)
   ;;(add-hook 'prog-mode-hook #'whitespace-mode)
-  (add-hook 'text-mode-hook #'display-line-numbers-mode)
-  (add-hook 'emacs-lisp-mode-hook #'hs-minor-mode)
-  (add-hook 'js-ts-mode-hook #'eglot-ensure) ; eglot auto load
-  (add-hook 'typescript-ts-mode-hook #'eglot-ensure)
-  (add-hook 'tsx-ts-mode-hook #'eglot-ensure)
 
-  (setq default-directory "~/Dev/")
-  (setq dired-kill-when-opening-new-dired-buffer t
-        explicit-shell-file-name "/usr/bin/bash"
+  (add-hook 'emacs-lisp-mode-hook #'hs-minor-mode)
+
+  (setq default-directory "~/Dev/"
+        dired-kill-when-opening-new-dired-buffer t)
+
+  (setq explicit-shell-file-name "/usr/bin/bash"
         shell-file-name "/usr/bin/bash")
   ;; Stop Emacs from overwriting OS clipboard
-  (setq select-enable-clipboard nil)
-  (setq select-enable-primary nil)
+  (setq select-enable-clipboard nil
+        select-enable-primary nil)
 
   (setq-default indent-tabs-mode nil
                 c-basic-offset 4
                 c-default-style "k&r"
                 tab-width 4)
+
+  ;; (add-to-list 'eglot-ignored-server-capabilities :documentOnTypeFormattingProvider)
+  
+  (with-eval-after-load 'eglot
+  (define-key eglot-mode-map (kbd "M-<down>") #'flymake-goto-next-error)
+  (define-key eglot-mode-map (kbd "M-<up>") #'flymake-goto-prev-error))
 
   (with-eval-after-load 'display-line-numbers
     (set-face-attribute 'line-number nil :background 'unspecified)
@@ -135,12 +147,6 @@
     (interactive)
     (insert (gui-get-selection 'CLIPBOARD)))
 
-  (defun my/open-init-file ()
-    "Open the initialization file."
-    (interactive)
-    (let ((emacs-path (expand-file-name "~/.config/emacs/post-init.el")))
-      (find-file emacs-path)))
-
   ;;(add-hook 'c-mode-hook 'eglot-ensure)
 
   (add-hook 'prog-mode-hook
@@ -148,11 +154,10 @@
               (local-set-key (kbd "<f5>") #'compile)))
 
   (keymap-set global-map "C-c n" #'display-line-numbers-mode)
-  (keymap-set global-map "C-c a" #'my/open-init-file)
-  (keymap-set global-map "C-c v" #'evil-mode)
-  (keymap-set global-map "C-c f r" #'recentf-open)
+  (keymap-set global-map "C-c r" #'recentf-open)
   (keymap-set global-map "C-S-c" #'my/copy-to-clipboard)
   (keymap-set global-map "C-S-v" #'my/paste-from-clipboard)
+  (keymap-set global-map "C-c ," #'duplicate-line)
 )
 ;; Treesitter
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
