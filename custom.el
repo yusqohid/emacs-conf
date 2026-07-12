036ddc6d68424aa75923d1666f899d887c647b24
yusqohid <yusqo975@gmail.com>
2026-07-12 18:38:23 +0700

    migrate to Vertico completion and clean up config

--- custom.el
+++ custom.el
@@ -5,16 +5,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("6963de2ec3f8313bb95505f96bf0cf2025e7b07cefdb93e3d2e348720d401425"
+   '("96f69937feec9ec89b879f19ea38c65ee0f85f418b41212bf8e5c75f9ca82bb6"
+     "6963de2ec3f8313bb95505f96bf0cf2025e7b07cefdb93e3d2e348720d401425"
      "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326"
      "9e5e0ff3a81344c9b1e6bfc9b3dcf9b96d5ec6a60d8de6d4c762ee9e2121dfb2"
      "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
      "e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7" default))
  '(package-selected-packages
-   '(auctex base16-theme company corfu diff-hl doom-themes ef-themes evil go-mode
-            hl-todo magit markdown-mode move-text multiple-cursors
-            nordic-night-theme org-bullets org-modern php-mode treemacs undo-fu
-            undo-fu-session vterm web-mode yasnippet))
+   '(auctex avy base16-theme company corfu diff-hl doom-themes ef-themes evil
+            go-mode hl-todo magit marginalia markdown-mode move-text
+            multiple-cursors nordic-night-theme odin-ts-mode orderless
+            org-bullets org-modern php-mode treemacs undo-fu undo-fu-session
+            vertico vterm web-mode yasnippet yasnippet-snippets))
+ '(package-vc-selected-packages
+   '((odin-ts-mode :url "https://github.com/Sampie159/odin-ts-mode")))
  '(whitespace-style
    '(face trailing tabs spaces lines missing-newline-at-eof empty indentation
           space-after-tab space-before-tab space-mark tab-mark)))
--- post-init.el
+++ post-init.el
@@ -4,25 +4,26 @@
 
 (use-package corfu
   :init
-  (global-corfu-mode)
-  (setq corfu-auto t
+    (setq corfu-auto t
         corfu-auto-delay 0.4
-        corfu-quit-no-match 'separator))
+        corfu-quit-no-match 'separator)
+    (global-corfu-mode)
+    (corfu-popupinfo-mode))
 
 (use-package move-text
   :bind (("M-<up>" . move-text-up)
          ("M-<down>" . move-text-down)))
 
-(use-package evil
-  :bind (("C-c v" . evil-mode)))
+(use-package evil :bind (("C-c v" . evil-mode)))
 
 (use-package vterm
   :config
   (setq vterm-shell (executable-find "fish")))
 
-;; (use-package yasnippet
-;;   :init
-;;   (yas-global-mode 1))
+(use-package yasnippet :init (yas-global-mode 1))
+(use-package vertico :init (vertico-mode))
+(use-package orderless :custom (completion-styles '(orderless basic)))
+(use-package marginalia :init (marginalia-mode))
 
 (use-package diff-hl
   :hook ((prog-mode . diff-hl-mode)
@@ -31,9 +32,6 @@
   :config
   (diff-hl-flydiff-mode)
   (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh))
-
-(use-package org-modern
-  :hook (org-mode . org-modern-mode))
 
 (use-package multiple-cursors
   :bind
@@ -75,14 +73,14 @@
   :config
   (undo-fu-session-global-mode))
 
-;; (use-package doom-themes
-;;   :config
-;;   ;; (load-theme 'doom-badger t)
-;;   (doom-themes-org-config))  ;; Corrects (and improves) org-mode's native fontification
-
-(use-package nordic-night-theme
+(use-package doom-themes
   :config
-  (load-theme 'nordic-night t))
+  (load-theme 'doom-badger t)
+  (doom-themes-org-config))  ;; Corrects (and improves) org-mode's native fontification
+
+;; (use-package nordic-night-theme
+;;   :config
+;;   (load-theme 'nordic-night t))
 
 (use-package ef-themes
   :init
@@ -93,24 +91,23 @@
   :config
   ;; All customisations here.
   (setq modus-themes-mixed-fonts t)
-  (setq modus-themes-italic-constructs t))
-  ;; (modus-themes-load-theme 'ef-dark)
+  (setq modus-themes-italic-constructs t)
+  ;;(modus-themes-load-theme 'modus-vivendi)
+  )
 
-(use-package hl-todo
-  :hook (prog-mode . hl-todo-mode))
+(use-package hl-todo :hook (prog-mode . hl-todo-mode))
 
-(use-package magit
-  :commands magit)
+(use-package magit :commands magit)
+
+(use-package avy :bind ("C-;" . 'avy-goto-char-2))
 
 (use-package emacs
   :ensure nil
   :init
-  ;;(add-to-list 'default-frame-alist '(font . "SauceCodePro Nerd Font-14"))
   (add-to-list 'default-frame-alist '(font . "Iosevka Output Minimal-14"))
   (column-number-mode 1)
   (repeat-mode 1)
   (electric-pair-mode 1)
-  (fido-vertical-mode 1)
   (which-key-mode 1)
   (pixel-scroll-precision-mode 1)
   (recentf-mode 1)
@@ -118,7 +115,6 @@
   (global-auto-revert-mode 1)
   (delete-selection-mode 1)
   (save-place-mode 1)
-  ;; (load-theme 'modus-vivendi t)
 
   :config
   (add-hook 'prog-mode-hook #'display-line-numbers-mode)
@@ -127,50 +123,44 @@
   (add-hook 'emacs-lisp-mode-hook #'hs-minor-mode)
 
   (setq default-directory "~/Dev/"
-        dired-kill-when-opening-new-dired-buffer t)
+        dired-kill-when-opening-new-dired-buffer t
+        completion-ignore-case t)
 
   (setq explicit-shell-file-name "/usr/bin/bash"
         shell-file-name "/usr/bin/bash")
   ;; Stop Emacs from overwriting OS clipboard
   (setq select-enable-clipboard nil
         select-enable-primary nil)
+  (setq debug-on-error t)
 
   (setq-default indent-tabs-mode nil
                 c-basic-offset 4
+                c-ts-mode-indent-offset 4
                 c-default-style "k&r"
                 tab-width 4)
+  (setq-default line-spacing 0.2)
 
   ;; (add-to-list 'eglot-ignored-server-capabilities :documentOnTypeFormattingProvider)
 
   (with-eval-after-load 'eglot
-  (define-key eglot-mode-map (kbd "M-n") #'flymake-goto-next-error)
-  (define-key eglot-mode-map (kbd "M-p") #'flymake-goto-prev-error))
-
-  (with-eval-after-load 'display-line-numbers
-    (set-face-attribute 'line-number nil :background 'unspecified)
-    (set-face-attribute 'line-number-current-line nil :background 'unspecified))
+    (keymap-set eglot-mode-map "M-n" #'flymake-goto-next-error)
+    (keymap-set eglot-mode-map "M-p" #'flymake-goto-prev-error))
 
   ;; CUSTOM FUNCTION
-  (defvar my/light-theme 'modus-operandi)
-  (defvar my/dark-theme 'modus-vivendi)
-
-  (defun my/toggle-theme ()
+(defun my/apply-line-number-faces (&rest _)
   (interactive)
-  (if (member 'modus-vivendi custom-enabled-themes)
-      (progn
-        (disable-theme 'modus-vivendi)
-        (load-theme 'modus-operandi t))
-    (progn
-      (disable-theme 'modus-operandi)
-      (load-theme 'modus-vivendi t)))
-
   (set-face-attribute 'line-number nil
-                      :background 'unspecified)
+                      :background 'unspecified
+                      :foreground "#5f5f5f")
   (set-face-attribute 'line-number-current-line nil
                       :background 'unspecified))
-  
-  (keymap-set global-map "<f6>" #'my/toggle-theme)
-  
+
+  (with-eval-after-load 'display-line-numbers
+    (my/apply-line-number-faces))
+
+  (advice-add 'load-theme :after #'my/apply-line-number-faces)
+  (keymap-set global-map "<f5>" #'modus-themes-toggle)
+
   (defun my/copy-to-clipboard ()
     (interactive)
     (if (region-active-p)
@@ -186,11 +176,8 @@
 
   ;;(add-hook 'c-mode-hook 'eglot-ensure)
 
-  (add-hook 'prog-mode-hook
-            (lambda ()
-              (local-set-key (kbd "<f5>") #'compile)))
-
   (keymap-set global-map "C-c n" #'display-line-numbers-mode)
+  (keymap-set global-map "C-c c" #'compile)
   (keymap-set global-map "C-c r" #'recentf-open)
   (keymap-set global-map "C-S-c" #'my/copy-to-clipboard)
   (keymap-set global-map "C-S-v" #'my/paste-from-clipboard)
@@ -198,7 +185,14 @@
   )
 
 ;; Treesitter
+(add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
+
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.[ch]\\'" . c-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
+
+(use-package odin-ts-mode
+  :vc (:url "https://github.com/Sampie159/odin-ts-mode")
+  :mode "\\.odin\\'")
