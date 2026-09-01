c8a48fe11d14c74102af0efab476eb8efec232e9
Yusqo Hid <yusqo975@gmail.com>
2026-09-01 19:30:32 +0700

    last-minimal

--- custom.el
+++ custom.el
@@ -7,31 +7,31 @@
  '(completion-category-overrides '((file (styles partial-completion))) nil nil "Customized with use-package orderless")
  '(custom-safe-themes
    '("96f69937feec9ec89b879f19ea38c65ee0f85f418b41212bf8e5c75f9ca82bb6"
-	 "6963de2ec3f8313bb95505f96bf0cf2025e7b07cefdb93e3d2e348720d401425"
-	 "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326"
-	 "9e5e0ff3a81344c9b1e6bfc9b3dcf9b96d5ec6a60d8de6d4c762ee9e2121dfb2"
-	 "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
-	 "e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7"
-	 default))
+     "6963de2ec3f8313bb95505f96bf0cf2025e7b07cefdb93e3d2e348720d401425"
+     "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326"
+     "9e5e0ff3a81344c9b1e6bfc9b3dcf9b96d5ec6a60d8de6d4c762ee9e2121dfb2"
+     "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
+     "e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7"
+     default))
  '(olivetti-style 'fancy)
  '(package-selected-packages
-   '(2048-game apheleia auto-dark avy consult copilot corfu diff-hl
-			   doom-themes evil expand-region ghostel gruvbox-theme
-			   hl-todo lorem-ipsum lua-mode magit marginalia
-			   markdown-mode mood-line move-text multiple-cursors
-			   nordic-night-theme odin-ts-mode olivetti orderless
-			   org-modern org-superstar perspective prisma-ts-mode
-			   undo-fu undo-fu-session vertico visual-fill-column
-			   web-mode yasnippet-snippets))
+   '(2048-game apheleia auto-dark avy company consult copilot corfu
+               diff-hl doom-themes evil expand-region ghostel
+               gruvbox-theme hl-todo lorem-ipsum lua-mode magit
+               marginalia markdown-mode mood-line move-text
+               multiple-cursors nordic-night-theme odin-ts-mode
+               olivetti orderless org-modern org-superstar perspective
+               prisma-ts-mode undo-fu undo-fu-session vertico
+               visual-fill-column web-mode yasnippet-snippets))
  '(package-vc-selected-packages
    '((copilot :url "https://github.com/copilot-emacs/copilot.el" :branch
-			  "main")
-	 (odin-ts-mode :url "https://github.com/Sampie159/odin-ts-mode")))
+              "main")
+     (odin-ts-mode :url "https://github.com/Sampie159/odin-ts-mode")))
  '(typescript-ts-mode-indent-offset 2)
  '(whitespace-style
    '(face trailing tabs spaces lines missing-newline-at-eof empty
-		  indentation space-after-tab space-before-tab space-mark
-		  tab-mark)))
+          indentation space-after-tab space-before-tab space-mark
+          tab-mark)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -1,6 +1,6 @@
-;; Initialize package sources
+;; Initialize package sources  -*- lexical-binding: t; -*-
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;; Load Custom file
@@ -10,13 +10,15 @@
 
 ;; Minimal UI adjustments
 (setq inhibit-startup-screen t)
-(tool-bar-mode		-1)
-(menu-bar-mode		-1)
-(scroll-bar-mode	-1)
-(tooltip-mode		-1)
-(load-theme 'modus-vivendi-deuteranopia t)
-(add-to-list 'default-frame-alist '(font . "Iosevka Nerd Font-16"))
-(set-frame-parameter nil 'alpha-background 85)
+(tool-bar-mode      -1)
+(menu-bar-mode      -1)
+(scroll-bar-mode    -1)
+(tooltip-mode       -1)
+(load-theme 'gruvbox-dark-hard t)
+(add-to-list 'default-frame-alist '(font . "JetBrainsMono Nerd Font-12"))
+;; (add-to-list 'default-frame-alist '(alpha-background . 85))
+(set-face-attribute 'line-number nil                :background 'unspecified)
+(set-face-attribute 'line-number-current-line nil   :background 'unspecified)
 (set-face-background 'fringe (face-background 'default))
 
 ;; QoL
@@ -26,59 +28,64 @@
 (electric-pair-mode 1)
 (recentf-mode 1)
 (savehist-mode 1)
-(delete-selection-mode 1)
 (save-place-mode 1)
+(delete-selection-mode 1)
 (global-auto-revert-mode 1)
 (column-number-mode 1)
 
 ;; Variable Customization
 (add-hook 'prog-mode-hook #'subword-mode)
-;; (add-hook 'prog-mode-hook #'display-line-numbers-mode)
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
 
 (setq duplicate-line-final-position 1
       completion-ignore-case t
       use-short-answers t
       shell-file-name "/usr/bin/bash"
-	  imenu-flatten 'annotation
-	  imenu-auto-rescan t
-	  use-package-enable-imenu-support t)
+      imenu-flatten 'prefix
+      imenu-auto-rescan t
+      use-package-enable-imenu-support t)
 
 ;; Delete by moving to trash in interactive mode
 (setq delete-by-moving-to-trash (not noninteractive)
-	  remote-file-name-inhibit-delete-by-moving-to-trash t)
+      remote-file-name-inhibit-delete-by-moving-to-trash t)
 
 (setq-default c-basic-offset 4
-			  c-ts-mode-indent-offset 4
-			  tab-width 4)
+              c-ts-mode-indent-offset 4
+              indent-tabs-mode nil
+              tab-width 4)
 
 ;; Global Keybindings
-(keymap-set global-map "C-c c"			#'compile)
-(keymap-set global-map "C-c g"			#'eglot)
-(keymap-set global-map "C-c n"			#'display-line-numbers-mode)
-(keymap-set global-map "C-c r"			#'recentf)
-(keymap-set global-map "C-M-S-<down>"	#'duplicate-dwim)
-(keymap-set global-map "C-x C-b"		#'ibuffer)
+(keymap-set global-map "C-c c"          #'compile)
+(keymap-set global-map "C-c g"          #'eglot)
+(keymap-set global-map "C-c n"          #'display-line-numbers-mode)
+(keymap-set global-map "C-c r"          #'recentf)
+(keymap-set global-map "C-c f"          #'consult-fd)
+(keymap-set global-map "C-M-S-<down>"   #'duplicate-dwim)
+(keymap-set global-map "C-x C-b"        #'ibuffer)
 
 ;; Treesitter
 (add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
+(add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
 
-(add-to-list 'auto-mode-alist '("\\.[jt]s\\'"	 . typescript-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.[jt]sx\\'"	 . tsx-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.rs\\'"	     . rust-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'"	     . php-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.go\\'"	     . go-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.[jt]s\\'"    . typescript-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.[jt]sx\\'"   . tsx-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.rs\\'"       . rust-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'"      . php-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.go\\'"       . go-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.odin\\'"     . odin-ts-mode))
+
+(add-hook 'c-ts-mode-hook 'eglot-ensure)
 
 ;; Builtin Packages
 (use-package dired
   :custom
-  (default-directory "~/Dev/")
   (dired-listing-switches "-alh")
   (dired-kill-when-opening-new-dired-buffer t)
   (dired-dwim-target t))
 
 (use-package org
   :hook ((org-mode . visual-line-mode)
-	 (org-mode . org-indent-mode))
+         (org-mode . org-indent-mode))
   :custom
   (org-directory "~/org/")
   (org-agenda-files '("~/org/agenda.org"))
@@ -86,11 +93,15 @@
   (add-to-list 'org-src-lang-modes '("rust" . rust-ts)))
 
 (use-package eglot
+  :config
+  (add-to-list 'eglot-ignored-server-capabilities :documentOnTypeFormattingProvider)
+  (add-to-list 'eglot-ignored-server-capabilities :documentHighlightProvider)
+  (add-to-list 'eglot-ignored-server-capabilities :semanticTokensProvider)
   :bind (:map eglot-mode-map
-	      ("C-c C-r" . eglot-rename)
-	      ("C-c C-a" . eglot-code-actions)
-	      ("C-c C-f" . eglot-format-buffer)
-	      ("C-c C-q" . eglot-shutdown)))
+              ("C-c C-r" . eglot-rename)
+              ("C-c C-a" . eglot-code-actions)
+              ("C-c C-f" . eglot-format-buffer)
+              ("C-c C-q" . eglot-shutdown)))
 
 (use-package flymake
   :bind (("M-n" . flymake-goto-next-error)
@@ -98,24 +109,28 @@
 
 ;; External Packages
 
-(use-package corfu
+(use-package company
   :ensure t
-  ;; :custom
-  ;; (setq corfu-auto t
-  ;;       corfu-quit-no-match 'separator)
+  :bind ("M-<tab>" . company-complete)
   :init
-  (global-corfu-mode)
-  (corfu-popupinfo-mode))
+  (global-company-mode 1))
 
+(use-package yasnippet     :init (yas-global-mode 1))
 (use-package expand-region :bind (("C-=" . er/expand-region)))
 (use-package multiple-cursors
-  :ensure t
-  :bind
-  (("C-S-<down>"     . mc/mark-next-like-this)
-   ("C-S-<up>"       . mc/mark-previous-like-this)
-   ("C-."            . mc/mark-next-like-this-symbol)
-   ("C-c C-S-<down>" . mc/mark-all-like-this)))
+  :bind (("C-S-<down>"     . mc/mark-next-like-this)
+         ("C-S-<up>"       . mc/mark-previous-like-this)
+         ("C-."            . mc/mark-next-like-this-symbol)
+         ("C-c C-S-<down>" . mc/mark-all-like-this)))
 
 (use-package move-text
-  :bind (("M-<up>"   . move-text-up)
-         ("M-<down>" . move-text-down)))
+  :bind (("M-<up>"         . move-text-up)
+         ("M-<down>"       . move-text-down)))
+
+(use-package diff-hl
+  :hook ((prog-mode  . diff-hl-mode)
+         (text-mode  . diff-hl-mode)
+         (dired-mode . diff-hl-dired-mode))
+  :config
+  (diff-hl-flydiff-mode)
+  (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh))
